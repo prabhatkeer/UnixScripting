99916d70e55ddfd9670c3f0602304d1bd3ef8e8b
Prabhat Keer <prabhat.keer@gmail.com>
2016-07-25 10:01:02 +1000

    adding new script

---
+++ deploy_prod2.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# 21-07-2016 Prabhat Keer < prabhat@ecconnect.com.au >
+# Script for Jeenee Gateway deploy
+
+function log {
+        echo "[`date`] $1"
+        echo -e "$1\n$2" > /var/tmp/jeenee_gw_deploy.log
+        exit $2
+}
+log "--------------------------------------------------------" 1
+
+backup () {
+if [ ! -d /home/jeenee/www/secure ]; then
+   log "Missing /home/jeenee/www/secure dir, not deploying, exiting" 2
+else
+   tar -czf ecgateway.`date +%Y%m%d`.tar.gz  ecgateway
+   log "Existing code is backed up in ecgateway.`date +%Y%m%d`.tar.gz in `pwd`" 1
+fi
+}
+
+## Deploying
+        backup;
+        cd ecgateway;
+        log "Deploying in `pwd`: " 1
+        #git pull &> /var/tmp/temp_output || { log "Git pull has failed. Please investigate" 2; }
+        /bin/cat /var/tmp/temp_output;
+log "-------------------------COMPLETE-----------------------" 1
+
+/bin/rm -f /var/tmp/temp_output
